655a86da4cb5d666c5b91d647b9b524ed461b3f1
Rohin T Narayan <narayan@physi.uni-heidelberg.de>
2012-09-07 22:13:51 +0200

    new functionality: set the run file as an argument to the program

--- main.cc
+++ main.cc
@@ -3,16 +3,24 @@
 
 #include <TTree.h>
 #include <TFile.h>
-int main()
+
+int main(int argc, char**argv)
 {
+    if(argv[1])
+    {
+        std::string daqFileName     = argv[1];
+        StreamMaker *stream = new StreamMaker(daqFileName);
+        stream->makeRawStream();
 
-    StreamMaker *stream = new StreamMaker("run_0191.run");
-    stream->makeRawStream();
-
-    TFile* f = new TFile("output.root","RECREATE");
-    TTree* rawTree = stream->getStream();
-    rawTree->Write();
-    f->Close();
-    
-    //stream->makeDataStream();
+        TFile* f = new TFile("output.root","RECREATE");
+        TTree* rawTree = stream->getStream();
+        rawTree->Write();
+        f->Close();
+        
+        //stream->makeDataStream();
+    }
+    else
+    {
+        std::cout <<"Expected a run file" <<std::endl;
+    }
 }
